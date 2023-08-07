dd05b79b8e4f6cf23a557fab1313fa961bcf9ef1
Jarrod Swords <jswords@blainsupply.com>
2023-08-07 16:38:10 -0400

    first feature

---
+++ TylerExample.Spec/WhenCreatingAnOrder.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace TylerExample.Spec;
+
+public class WhenCreatingAnOrder
+{
+    #region Requirements
+
+    [Fact]
+    public void ThenOrderHasAtLeastOneLineItem()
+    {
+        var order = new Order();
+
+        order.LineItems.Should().HaveCountGreaterThan(0);
+    }
+
+    #endregion
+}
---
+++ TylerExample/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace TylerExample;
+
+public class Order
+{
+    private readonly List<LineItem> _lineItems = new();
+
+    #region Creation
+
+    public Order()
+    {
+        _lineItems.Add(new());
+    }
+
+    #endregion
+
+    #region Implementation
+
+    public IReadOnlyList<LineItem> LineItems => _lineItems.AsReadOnly();
+
+    #endregion
+}
+
+public class LineItem
+{
+}
